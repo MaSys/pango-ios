1401631d0afe9aa5476fd02d9be1d729eb6745d4
Yaser Almasri <yaser@masys.mx>
2025-08-03 22:40:34 -0600

    Initial Commit

---
+++ Pangolinios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pangolinios
+//
+//  Created by Yaser Almasri on 03/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pangolinios/PangoliniosApp.swift
@@ -0,0 +1,17 @@
+//
+//  PangoliniosApp.swift
+//  Pangolinios
+//
+//  Created by Yaser Almasri on 03/08/25.
+//
+
+import SwiftUI
+
+@main
+struct PangoliniosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
